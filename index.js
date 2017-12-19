00ad8fd8a3a90ee7f2e972c929d87af6d5836c78
Tyler Collier <tyler.collier+github@devmounta.in>
2017-12-19 00:33:23 -0700

    For class

--- index.js
+++ index.js
@@ -6,7 +6,31 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/data', (req, res) => {
+  res.json({
+    user: 'pretend this is meaningful data, like URLs to pictures of the simpsons',
+  });
+});
+
 const port = 3005;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+
+
+
+
+
+
+
+
+function hasPassword(req, res, next) {
+  if (req.headers.password !== 't$rules') {
+    res.status(403).json({
+      message: 'You did not provide a password or the correct password'
+    });
+  } else {
+    next();
+  }
+}
