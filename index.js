400b863096465a43532c68abc3a0d7428412406d
Natalie Pulver <npulver00@gmail.com>
2018-12-11 11:09:06 -0700

    check

--- index.js
+++ index.js
@@ -6,17 +6,53 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/data', (req, res) => {
+app.use(checkPassword) //if you need it for all routes 
+// function checkPassword(req, res, next){
+//   if(req.query.password === "tylerrulessohard"){
+//     next();
+//     return;
+// }
+// res.status(403).json({message: "wrong password fool"})
+// }
+//or
+
+function checkPassword(req, res, next){
+  if(req.query.password !== "tylerrules"){
+    res.status(403).json({message: "wrong password fool"})
+    return;
+  }
+  next();
+}
+function checkAmazingness(req,res,next){
+  if(req.body.amazing === true){
+    next();
+    return;
+  }
+  res.status(400).json({message: "not the key word"})
+ 
+}
+
+app.get('/data', checkPassword,checkAmazingness, (req, res) => {
+    // if(req.query.params !== "tylerrulessohard"){
+    //   res.status(403).json({message: "wrong password"})
+    //   return;
+    // }
+
   res.json({
     someData: 'pretend this is meaningful data, like URLs to pictures of the simpsons',
   });
 });
 
-app.get('/more-data', (req, res) => {
+app.get('/more-data', checkPassword,checkAmazingness, (req, res) => {
+  // if(req.query.params !== "tylerrulesohard"){
+  //   res.status(403).json({message: "wrong password"})
+  //   return;
+  // }
   res.json({
     moreData: 'I wish this data were protected!',
   });
 });
+
 
 const port = 3005;
 app.listen(port, () => {
